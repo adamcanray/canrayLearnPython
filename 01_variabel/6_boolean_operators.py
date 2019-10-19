9fe236d0527fb115ff03e5de61765a14e156fec6
AdamCanray <adamcool180@gmail.com>
2019-10-19 10:23:38 +0700

    mengubah file 6_boolean_operator di folder 01_variable/

--- 01_variabel/6_boolean_operators.py
+++ 01_variabel/6_boolean_operators.py
@@ -32,4 +32,21 @@ l_than = nilai_satu < nilai_dua
 # print hasil nya
 print("3. apakah " + str(nilai_satu) + " itu kurang dari " + str(nilai_dua) + "?\njawab: " + str(l_than))
 
+print("\nTeknik Berantai:")
+# code diata masih menggunakan perbandingan biasa,
+# di python kita bisa mengunakan teknik berantai dalam menantukan perbandingan seperti kasus ini.
+# contoh:
+# kita ciptakan satu buah variabel dengan nilai 15
+nilai_tiga = 15
+# tampilkan nilai
+print("nilai_satu = " + str(nilai_satu) + "\nnilai_dua = " + str(nilai_dua) + "\nnilai_tiga = " + str(nilai_tiga))
+# True
+# kita akan membandingkan tiga buah variabel sekaligus atau lebih, teknik ini disebut teknik berantai
+berantai_true = nilai_dua < nilai_satu < nilai_tiga
+print("1. apakah (nilai_dua < nilai_satu) dan (nilai_satu < nilai_tiga)?\njawab: " + str(berantai_true))
+# False
+# kita akan membandingkan tiga buah variabel sekaligus atau lebih, teknik ini disebut teknik berantai
+berantai_false = nilai_satu < nilai_dua < nilai_tiga
+print("2. apakah (nilai_satu < nilai_dua) dan (nilai_dua < nilai_tiga)?\njawab: " + str(berantai_false) + " -- karena nilai_satu tidak kurang dari nilai_dua")
+
 # ---------------------------- end.
