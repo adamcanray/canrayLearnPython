d078578a3bf48d4c3376e5459b02014fa4f7bbbd
AdamCanray <adamcool180@gmail.com>
2019-10-19 20:32:29 +0700

    menambahkan file 7_string_method.py ke folder 02_string/

---
+++ 02_string/7_string_method.py
@@ -0,0 +1,21 @@
+# -----header
+# ada banyak method untuk memanipulasi sebuah tipe data string di dalam python
+# tetapi disini saya akan menjelaskan method lower() dan upper() saja.
+
+# ----- start reading
+# method yang digunakan di point ini:
+# --- 1. lower() --- ini akan mengubah string menjadi huruf kecil semua. penggunaan: string.lower()
+# --- 2. upper() --- ini akan mengubah string menjadi huruf besar semua. penggunaan: string.upper()
+# ----- end.
+
+# ----- start conding
+# Implementasi:
+# buat sebuah variabel string
+string = "Adam Canray"
+# tampilkan
+print(string)
+print(string.lower())
+print(string.upper())
+# ----- end.
+
+# note: di dalam python ada banyak sekali method, silahkan coba setiap method-nya dan pelajari fungsi-nya.
