99905b418d715088c8cd187eae0c3465e193edfc
AdamCanray <adamcool180@gmail.com>
2019-10-19 14:29:29 +0700

    menambahkan file 4_in_operators.py ke folder 02_string/

---
+++ 02_string/4_in_operators.py
@@ -0,0 +1,18 @@
+# ----- header
+# di python memiliki sebuah operator keanggotaan(Membership Operator) yaitu: in dan not in
+# Operator keanggotaan digunakan untuk menguji apakah urutan disajikan dalam suatu objek.
+# Operator ini akan mengecek apakah suatu nilai itu terkandung di dalam sebuah variabel atau tidak.
+
+# ----- start reading
+# Perhatikian:
+# * in -- akan mengembalikan nilai True jika suatu nilai itu terkandung di suatu variabel.
+# * not in -- akan mengembalikan nilai True jika suatu nilai itu tidak terkandung di suatu variabel.
+# ----- end.
+
+# ----- start conding
+# buat variabel string
+name = "Adam Canray"
+# tampilkan hasil
+print("Adam" in name) # apakah "Adam" terkandung di variabel name? : True
+print("Adam" not in name) # apakah "Adam" tidak terkandung di variabel name? : False
+# ----- end.
