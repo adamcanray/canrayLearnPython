20def403e97c843f2aeab7b301e6fe1aec57c58a
AdamCanray <adamcool180@gmail.com>
2019-10-19 17:04:38 +0700

    menambahkan file 6_char_escaping.py ke folder 02_variable/

---
+++ 02_string/6_char_escaping.py
@@ -0,0 +1,23 @@
+# ----- header
+# character escaping adalah teknik menggunakan backslash agar tidak escape(melarikan diri)
+# untuk menghindari tanda kutip tunggal atau ganda.
+
+# ----- start reading
+# Perhatikan:
+# * tanda kutip tunggal dapat digunakan dalam string dikutip ganda tanpa melarikan diri dan sebaliknya.
+# ----- end.
+
+# ----- start coding
+# implemenstasi:
+string1 = '"sweat" is an ice cream.'
+string2 = 'don\'t worry about \'me\''
+string3 = "don't worry about 'me'"~
+string4 ="The name of this ice-cream is \"Sweet'n'Tasty\""
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+# ----- end.
+
+# note: teman-teman silahkan ber-eksperimen sendiri dengan mencoba 
+# ----- mengganti kalimat yang memiliki kutip double atau single.
