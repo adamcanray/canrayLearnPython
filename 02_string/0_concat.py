72f251e9d4d38e357a399511ae8830f4e43f3c46
AdamCanray <adamcool180@gmail.com>
2019-10-19 12:52:19 +0700

    menambahkan folder 02_string/

---
+++ 02_string/0_concat.py
@@ -0,0 +1,9 @@
+# concat adalah sebuah teknik dimana kita menggabungkan dua buah string atau lebih menggunakan (+)
+
+# implementasi 
+# kita memiliki variabel berikut
+say = "Hello, my name is"
+name = "Adam"
+# lalu kita ingin menggabungkan variabel say dan variabel name
+print( say + " " + name )
+# output: Hello, my name is Adam
---
+++ 02_string/1_string_multiplication.py
@@ -0,0 +1,5 @@
+# Python mendukung perkalian sebuah string, dimana sebuah string bisa dikali 
+# sebanyak number yang kita masukan menggunakan perintah: string * 10
+# contoh:
+string = "hay saya adam\n"  # back-slash + n(\n) untuk break.
+print(string * 10)
