5befc8d8e242029d35a5c77bd4d3f541ea0b4a39
AdamCanray <adamcool180@gmail.com>
2019-10-21 00:40:15 +0700

    mengubah file 1_udentified_var.py di folder 01_variabel/

--- 01_variabel/1_udentified_var.py
+++ 01_variabel/1_udentified_var.py
@@ -1,13 +1,19 @@
-# Nama variabel hanya boleh berisi huruf, angka, dan / atau karakter garis bawah,
-# dan tidak bisa dimulai dengan angka.
+# ----- header
+# pendefinisian sebuah variabel dalam python.
 
+# ----- start reading
+# Perhatikan:
+# * Nama variabel hanya boleh berisi huruf, angka, dan / atau karakter garis bawah,
+# * dan tidak bisa dimulai dengan angka.
+# ----- end.
+
+
+# ----- start coding
 # pendefinisian variabel yang SALAH
 # dimulai dengan angka
 # 1adam = 100  # karena sebuah nama variabel itu tidak boleh dimulai dengan angka
-
 # salah: error
 # print(1adam)
-
 
 # pendefinisian variabel yang BENAR
 # dimulai dengan huruf besar
@@ -17,9 +23,17 @@ _adam = "aku variabel _adam"
 adam_canray = "aku variabel adam_canray"
 # selalu perhatikan huruf besar dan kecil, karena nama variabel sangat sensitif
 # variabel adam itu beda dengan Adam, karena huruf "a" pada awal var Adam adalah "A" (huruf besar)
-
 # benar: tidak error
 print(Adam)
 print(adam)
 print(_adam)
 print(adam_canray)
+
+# kata-kata yang tidak boleh dijadikan nama dari sebuah variabel
+# matikan komentar kode dibawah ini:
+# import keyword
+# print(keyword.kwlist)
+
+# ----- end.
+
+# note: silahkan pelajari lagi lebih lanjut sendiri tentang pendefinisian sebuah variabel.
