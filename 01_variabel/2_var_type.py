fa0ec9b9914e4e494008da19ece1f973047df1d8
AdamCanray <adamcool180@gmail.com>
2019-10-21 00:33:34 +0700

    mengubah file 2_var_type di folder 01_variabel/

--- 01_variabel/2_var_type.py
+++ 01_variabel/2_var_type.py
@@ -1,11 +1,14 @@
+# ----- header
 # dalam python ada dua buah type untuk number/nomor: integer dan float
 # sebenarnya ada lagi tipe yang namanya complex(tidak akan dijelaskan disini, silahkan pelajari sendiri)
 # perbedaan yang mencolok dari keduanya adalah:
 # float itu angka decimal yang mempunya dot/titik(.) contoh: 2.5
 # sedangkan integer itu hanya bilangan decimal yang bulat(tanpa titik, contoh: 1,25,100, dll)
-
+# ----- start reading
 # python memiliki built-in function type() -- untuk mengecek sebuah nilai/variabel itu type/tipe data nya apa
+# ----- end.
 
+# ----- start conding
 # integer number
 number = 10
 # keluarkan tipe dari variabel number
@@ -21,7 +24,7 @@ string = "Muhammad Adam Canrayneldy"
 # keluarkan tipe dari variabel string
 print(type(string))
 
-# array
+# array/list
 # di dalam sebuah array berisi banyak element
 # tipe data dari tiap-tiap element array itu bisa berbeda-beda
 array = ['adam', 1, "maulie", 2.4]
@@ -29,4 +32,22 @@ array = ['adam', 1, "maulie", 2.4]
 print(type(array))
 # output nya <class 'list'> --- karena dalam python array == list
 
-# ini lah tipe data yang ada di dalam python
+# tuple
+# tuple mirip seperti array
+# beda-nya adalah sebuah tuple nantinya tidak bisa kita tambah, ubah, atau hapus item-nya.
+tuple_ = ('adam', 1, "maulie", 2.4) 
+print(type(tuple_))
+
+# dictionary
+# dictionary mirip seperti array/list, hanya saja kita menggunakan sebuah key sebagai pengganti index.
+dict_ = {'apel' : 15, "jeruk" : 10, "mangga" : 5}
+print(type(dict_))
+
+# boolean
+# boolean hanya bernilai True dan False
+bool_ = True
+print(type(bool_))
+
+# ----- end.
+
+# note: ini lah tipe data yang ada di dalam python, silahkan kalian pelajari lagi.
