0b5007ebec358024f43cfb5442c325346f31243a
AdamCanray <adamcool180@gmail.com>
2019-10-22 14:50:57 +0700

    menambahkan file 1_parameters_and_arguments.py ke folder 06_functions/

---
+++ 06_functions/1_parameters_and_arguments.py
@@ -0,0 +1,31 @@
+# ----- header
+# parameter didefinisikan di dalam sebuah tanda (), setelah nama function.
+
+# ----- start reading
+# Perhatikan:
+# * sebuah argument adalah nilai yang dibutuhkan sebuah function sebagai value dari parameter.
+# * sebuah parameter bertindak sebagai nama variabel untuk argument yang diteruskan.
+# ----- end.
+
+# ----- start coding
+# Implementasi:
+
+# contoh 1:
+# definisikan sebuah function yang menerima 1 parameter
+def hay(say):
+    print(say)
+# jalanakan function dengan memberikan 1 argument
+hay("hi, nama saya Adam!")
+
+# contoh 2:
+# definisikan function yang menerima 2 parameter
+def kali(nilai_1,nilai_2):
+    print(nilai_1 * nilai_2)
+# jalankan function dengan memberikan 2 argument
+# nilai_1 = 10
+# nilai_2 = 20
+kali(10,20)
+
+# ----- end.
+
+# note: harus diingat bahwa urutan argument harus sama dengan parameter.
