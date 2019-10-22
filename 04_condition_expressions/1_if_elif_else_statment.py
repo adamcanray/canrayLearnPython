21d4d4d1d49028e97e4b175595ec83fc621e8999
AdamCanray <adamcool180@gmail.com>
2019-10-22 09:49:03 +0700

    menambahkan file 1_if_elif_else_statment.py ke folder 04_condition_expressions/

---
+++ 04_condition_expressions/1_if_elif_else_statment.py
@@ -0,0 +1,22 @@
+# ----- header
+# if, elif, else statment adalah sebuah cara kita untuk membuat suatu milai/hasil pada kondisi tertentu.
+
+# ----- start reading
+# Perhatikan:
+# * if statment adalah statment untuk mengeksekusi suatu perintah ketika kondisi-nya bernilai True.
+# * elif(else if) statment adalah statment untuk mengeksekusi suatu perintah ketika kondisi dari statment if itu tidak menghasilkan True.
+# * else statment adalah statment untuk pelengkap statment if.
+# ----- end.
+
+# ----- start coding
+ujian_harian = 80
+# if
+if ( ujian_harian > 90 ):
+    print("ujian kamu masuk kategory nilai A")
+elif ( ujian_harian > 75 ):
+    print("ujian kamu masuk kategory nilai B")
+else:
+    print("ujian kamu masuk kategory nilai C")
+# ----- end.
+
+# note: silahkan teman-teman bisa pelajari lebih lanjut tentang statment bersarang.
