97c19e920df734cbfbb84874386e5e50f0efdf7f
AdamCanray <adamcool180@gmail.com>
2019-10-22 10:46:13 +0700

    menambahkan file 1_while_loop.py ke folder 05_loops/

---
+++ 05_loops/1_while_loop.py
@@ -0,0 +1,22 @@
+# ----- header
+# while loop hampir mirip denga if statment: dia akan mengeksekusi code hanya jika kondisi-nya True.
+
+# ----- start reading
+# Perthatikan:
+# * kita harus membuat variabel sebagai nilai awal
+# * lalu kita baru bisa tentukan kondisi perulangan
+# * dan di dalam perulangan kita harus menambahkan nilai ke variabel setiap perulangannya.
+# * agar kondisi nantinya menhasilkan nilai False dan perulangan berhenti.
+# ----- end.
+
+# ----- start coding
+# buat variabel awal
+n_awal = 0
+# cek kondisi setiap perulangannya
+while ( n_awal < 10 ):
+    # jika kodisi bernilai True, maka akan menjalankan code dibawah ini
+    print("aku adalah = %d" % n_awal) # tampilkan nilai n_awal setipa perulangannya
+    n_awal +=1 # n_awal akan ditambah 1 setiap perulangannya
+# ----- end.
+
+# note: silahkan teman-teman pelajari lagi sendiri looping menggunakan while loop ini.
