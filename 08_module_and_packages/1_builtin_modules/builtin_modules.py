579f96eae2e97460f2cb8df806f803b0a34af8a5
AdamCanray <adamcool180@gmail.com>
2019-10-23 12:30:13 +0700

    menambahakan folder 1_builtin_modules/ ke folder 08_module_and_packages/

---
+++ 08_module_and_packages/1_builtin_modules/builtin_modules.py
@@ -0,0 +1,16 @@
+# ----- header
+# bulil-in module adalah module yang sudah disiapkan oleh python, kita hanya tinggal menggunakannya saja
+
+# ----- start reading
+# Perthatikan:
+# * untuk implemntasidibawah, saya akan mencontohkan yang paling dasar saja.
+# * yaitu mengeluarkan tanggal dan jam hari ini
+# ----- end.
+
+# ----- start coding
+# Implementasi:
+import datetime
+print(datetime.datetime.today())
+# ----- end.
+
+# note: silahkan teman-teman cari built-in module pada python dan pelajari sendiri, saran: https://docs.python.org/3/library/
